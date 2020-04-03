139d1eceb65896e8889ebdc4eb415b2114418cae
Jordyn Saltzman <jordyn.saltzman@gmail.com>
2020-04-03 11:41:50 -0400

    modified about section

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -20,18 +20,20 @@ const About = () => {
               I have an insatiable curiosity and love figuring out how things
               work.{" "}
             </strong>
-            Currently, I am building a solid foundation in web development at{" "}
+            Since October 2019 when I began my journey as a developer, I have
+            grown extremely passionate about code and aspire to be a great
+            full-stack engineer. Programming ignites my creative side and
+            inspires the designer in me, while simultaneously satisfying my love
+            of logic problems and abstract thinking. I am excited to be doing
+            what I love, and spend each day strengthening my skills, learning
+            new techniques, and devising creative solutions to problems.
+          </p>
+          <p>
+            I have just completed a 24-week certification course at{" "}
             <a className="bodyLink" href="https://bootcamp.wse.jhu.edu/">
-              The Coding Bootcamp at Johns Hopkins Engineering.
+              The Coding Bootcamp at Johns Hopkins Engineering
             </a>{" "}
-            Since October 2019 when I started this journey, I have built web
-            applications using HTML5, CSS3, JavaScript, jQuery, Bootstrap,
-            Express.js, ReactJS, Node.js, MongoDB, Mongoose.js, MySQL,
-            Sequelize.js, Command Line, REST APIs, Git and more. Outside of
-            class, I am taking several courses on Udemy, participating in
-            Hackathons, and getting lost in Stack Overflow. I am excited to be
-            taking the plunge into a new field that marries my love of problem
-            solving, design, and abstract thinking.
+            in April 2020. I look forward to what the future holds!
           </p>
           <a className="learnMore" href="/projects">
             <h3>View my work</h3>
